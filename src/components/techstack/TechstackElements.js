ef592b5c2b2dca0ca9bcd9116a7c90b641c7c8f9
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-09-09 13:25:01 -0700

    working on tech stack sizing between desktop and mobile 3

--- src/components/techstack/TechstackElements.js
+++ src/components/techstack/TechstackElements.js
@@ -36,8 +36,8 @@ export const TechstackHeader = styled.h2`
   -webkit-text-stroke: 0.25px black;
   background: transparent;
 
-  @media screen and (max-width: 520px), screen and (max-height: 600px) {
-    font-size: 1rem;
+  @media screen and (max-width: 752px), screen and (max-height: 600px) {
+    font-size: 1.2rem;
   }
 `;
 
@@ -67,8 +67,8 @@ export const Header = styled.h2`
     -webkit-text-stroke: 0.25px black;
   }
 
-  @media screen and (max-width: 520px), screen and (max-height: 600px) {
-    font-size: 0.8rem;
+  @media screen and (max-width: 752px), screen and (max-height: 600px) {
+    font-size: 1rem;
   }
 `;
 
@@ -85,7 +85,7 @@ export const Item = styled.li`
   -webkit-text-stroke: 1.25px black;
   background: transparent;
 
-  @media screen and (max-width: 520px), screen and (max-height: 600px) {
-    font-size: 0.8rem;
+  @media screen and (max-width: 752px), screen and (max-height: 600px) {
+    font-size: 1rem;
   }
 `;
