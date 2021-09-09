876eacefc50c80b0b501ecb6e6b646e8004052da
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-09-09 12:48:44 -0700

    changed media sizing for project links try 8

--- src/components/projects/ProjectsElements.js
+++ src/components/projects/ProjectsElements.js
@@ -86,8 +86,8 @@ export const ProjectImage = styled.img`
   }
 
   @media screen and (max-width: 500px), screen and (max-height: 600px) {
-    width: 18vh;
-    height: 18vh;
+    width: 21vh;
+    height: 21vh;
     padding: 0 0;
     margin: 0 0;
   }
